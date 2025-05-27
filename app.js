197bfc178d1c0ab2397726a04b760575490f8e55
ShakyBlake <shakyblakey123@gmail.com>
2025-05-28 09:45:21 +1200

    Updated database to add more accounts. Fixed a bug in the javascript

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ app.post("/profile/edit", (req, res) => {
 
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Dating site running on http://localhost:${PORT}`);
 });
